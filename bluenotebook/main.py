32c0ed403973520720ba5c27ee087ea2fdcce1f0
lephotographelibre <lephotographelibre@gmail.com>
2025-09-21 20:46:03 +0200

    V1.1.18 Mise en place Licence GNU GPLv3

--- bluenotebook/main.py
+++ bluenotebook/main.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 BlueNotebook - Éditeur de texte Markdown avec PyQt5
 
 Point d'entrée principal de l'application
