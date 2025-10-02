b0062251303acbcf79a6c27e7f57f3c2a2fd8df9
lephotographelibre <lephotographelibre@gmail.com>
2025-10-02 18:52:42 +0200

    V1.9.2 CSS Colors main.py et aide en ligne versions

--- bluenotebook/main.py
+++ bluenotebook/main.py
@@ -44,7 +44,7 @@ def main():
         app = QApplication(sys.argv)
 
         # Définir les informations de l'application
-        version = "1.9.1"
+        version = "1.9.2"
         app.setApplicationName("BlueNotebook")
         app.setApplicationVersion(version)
         app.setOrganizationName("BlueNotebook")
